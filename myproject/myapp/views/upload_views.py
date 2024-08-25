41f25b45333c6ac0a80ce9c6990fd662f06c61c4
EdenHarati <edenharati@pingidentity.com>
2024-08-25 13:11:25 +0300

    UI improvements

--- myproject/myapp/views/upload_views.py
+++ myproject/myapp/views/upload_views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.core.files.base import ContentFile
 from django.http import JsonResponse
-api_key = "sk-0"
+api_key = "sk-"
 
 class UploadCVFileView(LoginRequiredMixin, View):
 
